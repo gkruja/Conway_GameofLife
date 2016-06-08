62693ad9ec9bd119df45c55971c98a4368d54989
gkruja <gkruja2@gmail.com>
2016-06-08 19:08:12 -0400

    Revert "trewr"
    This reverts commit 433c540aa10ff7392564b41ee9be2d48cba662c1.

--- Conway_GameofLife/Conway_GameofLife/Form1.cs
+++ Conway_GameofLife/Conway_GameofLife/Form1.cs
@@ -27,6 +27,7 @@ namespace Conway_GameofLife
             timer.Interval = Settings.Default.TimeInterval;
             timer.Tick += Timer_Tick;
         }
+
         private void Timer_Tick(object sender, EventArgs e)
         {
 
@@ -56,6 +57,7 @@ namespace Conway_GameofLife
 
             
         }
+
         private void graphicsPanel1_Paint(object sender, PaintEventArgs e)
         {
 
@@ -91,6 +93,7 @@ namespace Conway_GameofLife
             universe[x, y] = !universe[x, y];
                 graphicsPanel1.Invalidate();
         }
+
         private void newToolStripButton_Click(object sender, EventArgs e)
         {
             Array.Clear(universe, 0, universe.Length);
@@ -100,18 +103,21 @@ namespace Conway_GameofLife
             toolStripStatusLabel1.Text = "Generation: " + generation.ToString();
             graphicsPanel1.Invalidate();
         }
+
         private void PlaytoolStripButton_Click(object sender, EventArgs e)
         {
             timer.Enabled = true;
             PlaytoolStripButton.Enabled = false;
             PausetoolStripButton.Enabled = true;
         }
+
         private void PausetoolStripButton_Click(object sender, EventArgs e)
         {
             timer.Enabled = false;
             PlaytoolStripButton.Enabled = true;
             PausetoolStripButton.Enabled = false;
         }
+
         private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Options temp = new Options();
@@ -126,11 +132,13 @@ namespace Conway_GameofLife
             }
 
         }
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             
             Settings.Default.Save();
         }
+
         private void SteptoolStripButton_Click(object sender, EventArgs e)
         {
             Timer_Tick(sender, e);
@@ -193,6 +201,15 @@ namespace Conway_GameofLife
         {
 
         }
+
+
+
+
+
+
+
+
+
 
 
 
